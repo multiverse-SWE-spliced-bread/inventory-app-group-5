d6fc2139bfe59efb931536080b19d34ce51fa72e
Danial Zahir <91701063+DanialZZahir@users.noreply.github.com>
2022-08-31 17:11:11 +0100

    fixed requests

--- BACKEND/server.js
+++ BACKEND/server.js
@@ -1,7 +1,24 @@
 const express = require('express')
 const app = express()
 const seed = require('./seedData/seed')
+const { Item } = require("./models");
 const {itemRoutes, saucesRoutes} = require('./routes');
+
+app.get('/', async (req, res) => {
+  const items = await Item.findAll();
+  res.send(items)
+})
+
+app.get('/:id', async (req, res) => {
+  const items = await Item.findByPk(req.params.id);
+  if(!items) {
+      res.status(404);
+    
+    }
+    res.send(items);
+})
+
+
 
 const serve = async (port) => {
     await seed() 
