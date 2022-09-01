b9f18d732dd67136b6d73ecbe1bf401ea799a9a1
Kieran Barker <29986418+kieranbarker@users.noreply.github.com>
2022-09-01 16:48:25 +0100

    Separate front end and back end

--- BACKEND/server.js
+++ BACKEND/server.js
@@ -1,30 +1,30 @@
-const express = require('express')
-const app = express()
-const seed = require('./seedData/seed')
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const seed = require("./seedData/seed");
 const { Item } = require("./models");
-const {itemRoutes, saucesRoutes} = require('./routes');
+const { itemRoutes, saucesRoutes } = require("./routes");
 
-app.get('/', async (req, res) => {
+app.use(cors());
+
+app.get("/", async (req, res) => {
   const items = await Item.findAll();
-  res.send(items)
-})
+  res.send(items);
+});
 
-app.get('/:id', async (req, res) => {
+app.get("/:id", async (req, res) => {
   const items = await Item.findByPk(req.params.id);
-  if(!items) {
-      res.status(404);
-    
-    }
-    res.send(items);
-})
-
-
+  if (!items) {
+    res.status(404);
+  }
+  res.send(items);
+});
 
 const serve = async (port) => {
-    await seed() 
-    app.listen(port, () => {
-      console.log(`Server listening at http://localhost:${port}`);
-    });
+  await seed();
+  app.listen(port, () => {
+    console.log(`Server listening at http://localhost:${port}`);
+  });
 };
 
 serve(5000);
