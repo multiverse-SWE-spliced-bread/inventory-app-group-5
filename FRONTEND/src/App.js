8f72398205ff6d800d6d4a029705338da6cbfb09
Kieran Barker <29986418+kieranbarker@users.noreply.github.com>
2022-09-01 16:59:49 +0100

    Render API data

--- FRONTEND/src/App.js
+++ FRONTEND/src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
+  const [items, setItems] = useState([]);
+  const [error, setError] = useState(false);
+
+  useEffect(() => {
+    async function getData() {
+      try {
+        const response = await fetch("http://localhost:5000");
+        const data = await response.json();
+        setItems(data);
+      } catch {
+        setError(true);
+      }
+    }
+    getData();
+  }, []);
+
+  if (items.length < 1) {
+    return (
+      <div className="App">
+        <p>Fetching inventory...</p>
+      </div>
+    );
+  }
+
+  if (error) {
+    return (
+      <div className="App">
+        <p>There was a problem.</p>
+      </div>
+    );
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {items.map((item) => (
+        <h2 key={crypto.randomUUID()}>{item.title}</h2>
+      ))}
     </div>
   );
 }
